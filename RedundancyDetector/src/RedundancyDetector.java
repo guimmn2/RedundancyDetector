1400703e187b13007866c60569233a5cdbaab022
guimmn2 <gmnss@protonmail.com>
2021-05-13 18:02:32 +0100

    fiz função que compara 2 subStrs do mesmo tamanho, dada a base da 1ª e da 2ª

--- RedundancyDetector/src/RedundancyDetector.java
+++ RedundancyDetector/src/RedundancyDetector.java
@@ -3,106 +3,43 @@ import java.math.*;
 
 public class RedundancyDetector {
 	
-	/* IDEIA GERAL: Dividir String em substrings incrementalmente mais pequena, enquanto não se encontra a
-	 * maior string redundante possível.
-	 * 
-	 * I - Dividir String em substrings e guardá-las numa estrutura (talvez uma queue, ou um binary heap???)
-	 * Ia - Opcionalmente guardar apenas as que se repetem ???
-	 * IIa - Opcionalmente ordenar se custo de operação valer a pena, para ser mais fácil procurar e comparar substrings
-	 * IIb - Comparar substrings até encontrar um match.
-	 * III - Devolver substring (atenção: verificar caso em que há mais de uma string redundante com o mesmo tamanho)
+	/* IDEIA GERAL: Dividir String em substrings incrementalmente mais pequenas, começando por dividir ao meio. 
+	 * Começar por comparar primeiro caracter de cada substring resultante e avançar apenas conforme haja um "match" de chars
+	 * caso contrário compara-se com a próxima substring possível e por aí em diante até todos os chars das subs serem iguais
 	 */
 	
 	public static void main(String[] args){
 		
 		Scanner in = new Scanner(System.in);
-		
-		//pede a String
 		String test = in.nextLine();
-		int factor = in.nextInt();
 		
-		//System.out.println(read(test,0,3));
+		char[] s = test.toCharArray();
+	
+		System.out.println(compareSubstrings(s,1,3,2));
 		
-		Queue<String> q = divideInto(factor,test);
-		System.out.println(q.toString());
-		
-//		System.out.println(read(test,4,3));
 		
 		in.close();
 		
 		
 	}
 	
-	//função de teste para obter um "feel" da computação envolvida para dividir a string em todas as substrings
-	private static Queue<String> divideInto(int factor, String s){
+	
+	//compara substrings do mesmo tamanho, caracter a caracter, se forem iguais devolve true.
+	/*
+	 * s = String original convertida em array de chars
+	 * base 1 = indice do inicio da 1ª substring
+	 * base 2 = " da 2ª
+	 * size = tamanho das substrings
+	 */
+	private static boolean compareSubstrings(char[] s, int base1, int base2, int size){
+				
+		if(base2 + size > s.length || base1 + size > s.length) throw new IllegalStateException("Out of bounds!");
 		
-		char[] aux = s.toCharArray();
-		int subStrSize = (int)(aux.length / factor);
-		System.out.println(subStrSize);
-		
-		Queue<String> q = new LinkedList<String>();
-		
-		for(int i = 0; i < aux.length - subStrSize; i+= subStrSize){ 
-			System.out.println("iterator is: " + i);
-			String temp = read(s,i,subStrSize);
-			System.out.println("adding to queue: " + temp);
-			q.add(temp);
+		for(int i = base1; i <= size; i++){
+			System.out.println(s[i] + " " + s[i+size]);
+			if(s[i] != s[i+size]) return false;
 		}
-		return q;
-		
+		return true;	
 	}
-	
-	
-	
-	
-	//devolve uma substr começada em base com tamanho offset.
-	private static String read(String s, int base, int offset){
-		
-		char[] arr = s.toCharArray();
-		String res = "";
-		Queue<Character> q = new LinkedList<Character>();
-		
-		if(offset < 0) throw new IllegalStateException("offset tem que ser positivo!");
-		else if(base > arr.length - 1) throw new IllegalStateException("base fora do array!");
-		
-		else if(base + offset > arr.length) throw new IllegalStateException("out of bounds!");
-			
-		else {
-			
-//			System.out.println("AGORA ESTAMOS NO ELSE QUE N TEM IF!");
-		
-		for(int i = base; i < base + offset; i++){
-			//System.out.println("adding: " + arr[i]);
-			q.add(arr[i]);
-		}
-		
-//		res = q.toString();
-//		System.out.println(res);
-		
-		char[] aux = new char[q.size()];
-		//System.out.println("queue size @read() is: " + q.size());
-		int i = 0;
-		
-		while(q.iterator().hasNext()){
-			aux[i] = q.remove();
-			/*System.out.println(aux[i]);*/ i++;
-		}
-		
-		res = String.valueOf(aux);
-		
-		}
-		return res;
-	}
-	
-	
-	
-
-	
-	
-	
-	
-
-	
-	
 	
 }
