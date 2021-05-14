8253a8a9adad2b109f72a56ad29ea598d1dd9387
guimmn2 <gmnss@protonmail.com>
2021-05-14 19:59:11 +0100

    depois de uma tarde a dar flex a tentar ser um chad recursivo, acabámos por usar um par de fors

--- RedundancyDetector/src/RedundancyDetector.java
+++ RedundancyDetector/src/RedundancyDetector.java
@@ -12,17 +12,12 @@ public class RedundancyDetector {
 
 		Scanner in = new Scanner(System.in);
 		String test = in.nextLine();
-		
+
 
 		char[] s = test.toCharArray();
-//		System.out.println(String.valueOf(s));
-
-
-		//System.out.println(compareSubstrings(s,0,2,2));
-
-		System.out.println(LRSubstring(test));
-		//System.out.println(seekLRSubstring(test,0,2));
-
+		
+		//iterateAndCompare(s);
+		System.out.println(seekLRSubstring(test));
 
 		in.close();
 
@@ -39,81 +34,111 @@ public class RedundancyDetector {
 	 */
 	private static boolean compareSubstrings(char[] s, int base1, int base2, int size){
 
-		if(base2 + size > s.length || base1 + size > s.length) throw new IllegalStateException("Out of bounds!");
+		//if(base2 + size > s.length || base1 + size > s.length) throw new IllegalStateException("Out of bounds!");
 
 		int j = base2;
 		int lim = base1 + size - 1;
-		
+
 		for(int i = base1; i <= lim && j < s.length; i++){
-			System.out.println("Comparing: " + s[i] + " with " + s[j] );
+			//System.out.println("Comparing: " + s[i] + " with " + s[j] );
 			if(s[i] != s[j]) return false;
 			j++;
 		}
-		System.out.println("TRUE");
+		//System.out.println("TRUE");
 		return true;	
 	}
 
 
-	private static String LRSubstring(String s){
-		//System.out.println()
-		return seekLRSubstring(s,0,(int)(s.length()/2));
-	}
+//	private static String iterateAndCompare(char[] arr){
+//		
+//		String res = "";
+//		int size = (int)(arr.length/2);
+//		int limit = size;
+//
+//		while(size > 1){
+//			for(int i = 0; i != limit; i++){
+//				for(int j = i + size; j < arr.length - size; j++){
+//					
+//					System.out.println("i,j = " + i + " " + j);
+////					if(compareSubstrings(arr,i,j,size)) res = foundString(arr,i,size);
+//					System.out.println("cheguei aqui!");
+//				}
+//				limit = i + size - 1;
+//			}
+//			size --;
+//		}
+//		return res;
+//	}
 
-	private static String seekLRSubstring(String s, int base1, int size){
 
-		char[] aux = s.toCharArray();
-		//System.out.println("char array: " + String.valueOf(aux));		
-		int N = aux.length;
 
-		int subSize = size; //o potencial tamanho máximo de uma substring redundante é metade; ex: "baba" -> "ba"
-		int i = base1;
-		int j = i + subSize; //subSize = 3
-
-		System.out.println("RECURSIVIDADE --> i: " + i + " j: " + j + " subSize: " + subSize);
-
-		try {
-			while(true){
-				if (compareSubstrings(aux,i,j,subSize)){ System.out.println("CALLED FOUNDSTRING, args: " + String.valueOf(aux) + " " + i + " " + subSize); return foundString(aux,i,subSize);}
-				else j++; System.out.println("j: " + j);
+		private static String seekLRSubstring(String s){
+	
+			String res = "";
+			char[] aux = s.toCharArray();
+			//System.out.println("char array: " + String.valueOf(aux));		
+			int N = aux.length;
+	
+			int subSize = (int)(N/2); //o potencial tamanho máximo de uma substring redundante é metade; ex: "baba" -> "ba"
+//			int i = 0;
+//			int j = i + subSize; //subSize = 3
+			
+			while(subSize >= 1){
+				
+				//System.out.println("i, j = " + i + " " + j);
+				
+				int lim1 = N - (2 * subSize);
+				int lim2 = N - subSize;
+				
+				for(int i = 0; i <= lim1; i++){
+					for(int j = i + subSize; j <= lim2; j++){
+						//System.out.println("i, j = " + i + " " + j);
+						//System.out.println("SIZE IS: " + subSize);
+						if(compareSubstrings(aux,i,j,subSize)){ return foundString(aux,i,subSize); }
+					}
+				}
+				subSize--;
 			}
-		} catch(Exception e) {
-			System.out.println(e);
-			System.out.println("APANHEI A EXCEPÇÃO");
-			if(i != N - (2 * subSize)) { i++;  return seekLRSubstring(s,i,subSize);/*System.out.println("index do i: " + i + " " + "indice máximo do i: " + (N - (2 * subSize) - 1))*/ }
-			else { subSize --; i = 0; return seekLRSubstring(s,i,subSize); }
+			return res;
+//			try{
+//				while(true){
+//					
+//					if(compareSubstrings(aux,i,j,subSize)) return foundString(aux,i,subSize);
+//					j++;
+//				}
+//				
+//			} catch(Exception e){
+//				
+//					while(i != N - (2 * subSize)){
+//						i++;
+//						for(int k = j; k < N - subSize; j++){
+//							if(compareSubstrings(aux,i,j,subSize)) return foundString(aux,i,subSize);
+//						}
+//					}
+//					subSize --;
+//					i = 0;
+//			}
+	
 		}
-
-	}
 
 
 	private static String foundString(char[] s,int base, int size){
 
-//		Queue<Character> aux = new LinkedList<Character>();
-//		
-//		for(int i = base; i < size; i++){
-//			System.out.println("Adding: " + s[i]);
-//			aux.add(s[i]);
-//			}
-//		
-//		String res = aux.toString();
-//		System.out.println(res);
-//		
-//		return res;
-		System.out.println("ENTREI NO FOUNDSTRING!");
-		System.out.println(String.valueOf(s) + " " + base + " " + size);
+		//System.out.println("ENTREI NO FOUNDSTRING!");
+		//System.out.println(String.valueOf(s) + " " + base + " " + size);
 		char[] aux = new char[size];
 		int j = 0;
 		int lim = base + size - 1;
-		
+
 		for(int i = base; i <= lim; i++){
-			System.out.println("Printing in aux: " + s[i]);
+			//System.out.println("Printing in aux: " + s[i]);
 			aux[j] = s[i];
-			System.out.println("Saved char: " + aux[j]);
+			//System.out.println("Saved char: " + aux[j]);
 			j++;
 		}
-		
+
 		String res = String.valueOf(aux);
-		System.out.println(res);
+		//System.out.println(res);
 		return res;
 
 	}
