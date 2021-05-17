2749f80d35d8e8dd4213064b63a7923bb36fbdef
Vasco Araújo <vasconparaujo@gmail.com>
2021-05-17 23:04:09 +0100

    Final Version

--- RedundancyDetector/src/RedundancyDetector.java
+++ RedundancyDetector/src/RedundancyDetector.java
@@ -1,40 +1,86 @@
+import java.io.*;
 import java.util.*;
 //import java.math.*;
 
 public class RedundancyDetector {
 
-	/* IDEIA GERAL: Dividir String em substrings incrementalmente mais pequenas, comeÁando por dividir ao meio. 
-	 * ComeÁar por comparar primeiro caracter de cada substring resultante e avanÁar apenas conforme haja um "match" de chars
-	 * caso contr·rio compara-se com a prÛxima substring possÌvel e por aÌ em diante atÈ todos os chars das subs serem iguais
+	/* IDEIA GERAL: Dividir String em substrings incrementalmente mais pequenas, come√ßando por dividir ao meio.
+	 * Come√ßar por comparar primeiro caracter de cada substring resultante e avan√ßar apenas conforme haja um "match" de chars
+	 * caso contr√°rio compara-se com a pr√≥xima substring poss√≠vel e por a√≠ em diante at√© todos os chars das subs serem iguais
 	 */
+	public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args){
+		File fx = null;
+		InputStream istream = null;
+		fx = new File(args[0]);
+		istream = new FileInputStream(fx);
+		Queue<Character> q = new LinkedList<Character>();
+		try {
 
-		Scanner in = new Scanner(System.in);
-		String test = in.nextLine();
+			Reader reader = new InputStreamReader(istream);
+			int c;
 
-
-		char[] s = test.toCharArray();
-
-		//iterateAndCompare(s);
-		System.out.println(seekLRSubstring(test));
-
-		in.close();
-
-
+			while ((c = reader.read()) != -1) {
+				if (c == ' ')
+					c = '-';                 // just replace spaces with dash
+				q.add((char) c);
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Oops");
+		}
+		char[] aux = new char[q.size()];
+		int i = 0;
+		while(q.iterator().hasNext()){
+			aux[i]=q.remove();
+			i++;
+		}
+		String res = String.valueOf(aux);
+		//System.out.println(String.valueOf(aux));
+		res=seekLRSubstring(res);
+		System.out.println("Size of redundant string: " +res.length());
+		System.out.println("Longest redundant string: " +res);
 	}
 
+
+	private static String seekLRSubstring(String s){
+
+		String res = "";
+		char[] aux = s.toCharArray();
+		int N = aux.length;
+
+		int subSize = (int)(N/2); //o potencial tamanho m√°ximo de uma substring redundante √© metade; ex: "baba" -> "ba"
+
+		while(subSize >= 1){
+
+			int lim1 = N - (2 * subSize);
+			int lim2 = N - subSize;
+
+			for(int i = 0; i <= lim1; i++){
+				int k = lim2;
+				for(int j = i + subSize; j <= lim2/2+1; j++){
+					//System.out.println("i: "+i+"| j: "+j+"| k: "+k);
+					if(compareSubstrings(aux,i,j,subSize) || compareSubstrings(aux,i,k,subSize)){
+						return foundString(aux,i,subSize);
+					}
+					k--;
+				}
+			}
+			subSize--;
+		}
+		System.out.println("No string found!");
+		return res;
+
+	}
 
 	//compara substrings do mesmo tamanho, caracter a caracter, se forem iguais devolve true.
 	/*
 	 * s = String original convertida em array de chars
-	 * base 1 = indice do inicio da 1™ substring
-	 * base 2 = " da 2™
+	 * base 1 = indice do inicio da 1¬™ substring
+	 * base 2 = " da 2¬™
 	 * size = tamanho das substrings
 	 */
 	private static boolean compareSubstrings(char[] s, int base1, int base2, int size){
-
-		//if(base2 + size > s.length || base1 + size > s.length) throw new IllegalStateException("Out of bounds!");
 
 		int j = base2;
 		int lim = base1 + size - 1;
@@ -44,72 +90,31 @@ public class RedundancyDetector {
 			j++;
 		}
 		//System.out.println("TRUE");
-		return true;	
+		return true;
 	}
 
 
+	//fun√ß√£o que devolve string dado um array de chars, uma base e um tamanho
+	//parte do indice base e l√™ size caracteres, inclusive a base
+	private static String foundString(char[] s,int base, int size){
 
+		//System.out.println("ENTREI NO FOUNDSTRING!");
+		//System.out.println(String.valueOf(s) + " " + base + " " + size);
+		char[] aux = new char[size];
+		int j = 0;
+		int lim = base + size - 1;
 
-	private static String seekLRSubstring(String s){
+		for(int i = base; i <= lim; i++){
+			//System.out.println("Printing in aux: " + s[i]);
+			aux[j] = s[i];
+			//System.out.println("Saved char: " + aux[j]);
+			j++;
+		}
 
-		String res = "";
-		char[] aux = s.toCharArray();
-		//System.out.println("char array: " + String.valueOf(aux));		
-		int N = aux.length;
+		String res = String.valueOf(aux);
+		//System.out.println(res);
+		return res;
 
-		int subSize = (int)(N/2); //o potencial tamanho m·ximo de uma substring redundante È metade; ex: "baba" -> "ba"
-		//			int i = 0;
-		//			int j = i + subSize; //subSize = 3
-
-		while(subSize >= 1){
-
-			//System.out.println("i, j = " + i + " " + j);
-
-			int lim1 = N - (2 * subSize);
-			int lim2 = N - subSize;
-			
-//			System.out.println("SubSize "+subSize);
-//			System.out.println("lim1 "+lim1);
-//			System.out.println("lim2 "+lim2);
-
-
-			for(int i = 0; i <= lim1; i++){
-				int k = lim2;
-				for(int j = i + subSize; j <= lim2/2+1; j++){
-					//System.out.println("i: "+i+"| j: "+j+"| k: "+k);
-					if(compareSubstrings(aux,i,j,subSize) || compareSubstrings(aux,i,k,subSize)){ 
-						return foundString(aux,i,subSize); 
-					}
-					k--;	
-				}
-			}
-		subSize--;
 	}
-	System.out.println("No string found!");
-	return res;
-	
-}
-
-
-private static String foundString(char[] s,int base, int size){
-
-	//System.out.println("ENTREI NO FOUNDSTRING!");
-	//System.out.println(String.valueOf(s) + " " + base + " " + size);
-	char[] aux = new char[size];
-	int j = 0;
-	int lim = base + size - 1;
-
-	for(int i = base; i <= lim; i++){
-		//System.out.println("Printing in aux: " + s[i]);
-		aux[j] = s[i];
-		//System.out.println("Saved char: " + aux[j]);
-		j++;
-	}
-
-	String res = String.valueOf(aux);
-	//System.out.println(res);
-	return res;
-
-}
 
 }
